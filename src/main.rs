69f14b0cf97da62e447f02ae1f95feda3dc2ddb6
Andrew Cowie <istathar@gmail.com>
2025-03-18 16:26:40 +1100

    Import skeleton command-line argument handling from tracer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use clap::{Arg, ArgAction, Command};
+
 fn main() {
-    println!("Hello, world!");
+    const VERSION: &str = concat!("v", env!("CARGO_PKG_VERSION"));
+
+    let matches = Command::new("tracer")
+            .version(VERSION)
+            .propagate_version(true)
+            .author("Andrew Cowie")
+            .about("Retrieve workflow and run from GitHub Actions and send to OpenTelemetry as spans and traces.")
+            .disable_help_subcommand(true)
+            .disable_help_flag(true)
+            .disable_version_flag(true)
+            .arg(
+                Arg::new("help")
+                    .long("help")
+                    .long_help("Print help")
+                    .global(true)
+                    .hide(true)
+                    .action(ArgAction::Help))
+            .arg(
+                Arg::new("version")
+                    .long("version")
+                    .long_help("Print version")
+                    .global(true)
+                    .hide(true)
+                    .action(ArgAction::Version))
+            .arg(
+                Arg::new("workflow")
+                    .long("workflow")
+                    .action(ArgAction::Set)
+                    .default_value("check.yaml")
+                    .help("Name of the GitHub Actions workflow to present as a trace. The default workflow used if unspecified is check.yaml"))
+            .get_matches();
+
+    let workflow = matches.get_one::<String>("workflow").unwrap().to_string();
+
+    println!("{}", workflow);
 }
