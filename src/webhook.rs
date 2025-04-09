ecc6339b7f7f57b925e85de96ff1265c64595c25
Andrew Cowie <istathar@gmail.com>
2025-04-09 14:26:37 +1000

    Extract relevant fields from webhook request

--- src/webhook.rs
+++ src/webhook.rs
@@ -4,6 +4,7 @@
 use anyhow::Result;
 use axum::Json;
 use axum::{Router, routing::get};
+use serde::Deserialize;
 use tracing::debug;
 use tracing::info;
 
@@ -21,10 +22,77 @@ pub(crate) async fn run_webserver(port: u32) -> Result<()> {
     Ok(())
 }
 
+#[derive(Deserialize)]
+struct RequestPayload {
+    action: String,
+    organization: WebhookOrganization,
+    repository: WebhookRepository,
+    workflow_run: WebhookWorkflowRun,
+}
+
+#[derive(Deserialize)]
+struct WebhookOrganization {
+    login: String,
+}
+
+#[derive(Deserialize)]
+struct WebhookRepository {
+    name: String,
+}
+
+#[derive(Deserialize)]
+struct WebhookWorkflowRun {
+    actor: WebhookActor,
+    conclusion: String,
+    display_title: String,
+    event: String,
+    head_branch: String,
+    path: String,
+}
+
+#[derive(Deserialize)]
+struct WebhookActor {
+    login: String,
+}
+
 async fn hello_world() -> &'static str {
     "Hello world!"
 }
 
-async fn receive_post(Json(value): Json<serde_json::Value>) {
-    println!("{}", serde_json::to_string_pretty(&value).unwrap())
+async fn receive_post(Json(payload): Json<RequestPayload>) {
+    let path = payload
+        .workflow_run
+        .path;
+    let filename = path
+        .split('/')
+        .last()
+        .unwrap();
+
+    println!(
+        "{}: {}/{} {} \"{}\" by {} via {} for {}: {}",
+        payload.action,
+        payload
+            .organization
+            .login,
+        payload
+            .repository
+            .name,
+        filename,
+        payload
+            .workflow_run
+            .display_title,
+        payload
+            .workflow_run
+            .actor
+            .login,
+        payload
+            .workflow_run
+            .event,
+        payload
+            .workflow_run
+            .head_branch,
+        payload
+            .workflow_run
+            .conclusion
+    );
 }
